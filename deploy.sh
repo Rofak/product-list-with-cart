d201db3ca3c907464d3b1888d5575cdec9e8a7b6
Rofak <rofak.man@ascendcorp.com>
2024-11-05 09:15:28 +0700

    git hub page

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+
+# build
+yarn generate
+
+# navigate into the build output directory
+cd dist
+
+# initialize a new Git repo and deploy
+git init
+git add -A
+git commit -m 'deploy'
+
+# push to the `gh-pages` branch
+git push -f git@github.com:Rofak/product-list-with-cart.git master:gh-pages
+
+cd -
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
-  ssr:false
+  ssr:false,
+  app: {
+    baseURL: '/product-list-with-cart/'  // Replace with your repo name
+  }
 })
